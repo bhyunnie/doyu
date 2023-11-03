8c3f891dbd55da76a941e644ec5381b397ce9d25
bhyunnie <dev.bhyunnie@gmail.com>
2023-11-04 00:16:44 +0900

    :tada: INITIATE DOYU PROJECT

---
+++ src/main/kotlin/com/doyu/kr/DoyuApplication.kt
@@ -0,0 +1,11 @@
+package com.doyu.kr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoyuApplication
+
+fun main(args: Array<String>) {
+    runApplication<DoyuApplication>(*args)
+}
---
+++ src/test/kotlin/com/doyu/kr/DoyuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.doyu.kr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoyuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
