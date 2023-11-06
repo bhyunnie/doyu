e88ae571c1eaacb72d1a706ebaabfcdd42ea83bf
bhyunnie <dev.bhyunnie@gmail.com>
2023-11-07 01:04:30 +0900

    [FIX] 코틀린 스프링 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.doyu"
@@ -22,8 +23,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/doyu/kr/DoyuApplication.kt
+++ src/main/kotlin/com/doyu/kr/DoyuApplication.kt
@@ -1,7 +1,9 @@
 package com.doyu.kr
 
+import jakarta.persistence.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootApplication
 class DoyuApplication
@@ -9,3 +11,20 @@ class DoyuApplication
 fun main(args: Array<String>) {
     runApplication<DoyuApplication>(*args)
 }
+
+@Transactional
+class Service
+
+@Entity
+@Table
+class Person (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id : Long?,
+
+        @Column
+        var name : String?,
+
+        @Column
+        var age: Int
+)
